2b5f5574f167883efdbe0f9d3192fc369cfc2d63
719Ben <ben.williams@colorado.edu>
2016-10-09 12:08:37 -0600

    Added events & UI

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask, request, session, redirect, render_template
-from flask.ext.api import status
-from werkzeug.serving import run_simple
 import bcrypt
 import pymysql.cursors
 import json
+import os
+import sys
+from flask import Flask, request, session, redirect, render_template
+from flask_api import status
+from werkzeug.serving import run_simple
+from datetime import timezone, datetime
+
+
+def utc_to_local(utc_dt):
+    return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 
 # open up the config file
@@ -50,16 +61,105 @@ def hashPassword(passwrd):
     return bcrypt.hashpw(passwrd.encode(), bcrypt.gensalt())
 
 # wraper to check hashed passwords, returns a bool
+
+
 def checkPassword(passwrd, hashedPass):
     return hashedPass.encode() == bcrypt.hashpw(passwrd.encode(),
                                                 hashedPass.encode())
 
+
 @app.route("/", methods=["GET"])
 def index():
     if isLoggedin():
-        return render_template('index.html')
+
+        connection = createDBConnection()
+
+        cursor = connection.cursor()
+        sql = "SELECT users.email, chapters.name FROM users, chapters WHERE users.id=%s AND chapters.id = users.chapterID"
+        cursor.execute(sql, (session["userID"]))
+        results = cursor.fetchone()
+
+        sql = "SELECT id, name, chapterID FROM events WHERE chapterID = %s ORDER BY created_on DESC"
+        cursor.execute(sql, (session["chapterID"]))
+        event_list = cursor.fetchall()
+
+        try:
+            eventID = request.args.get("event_id")
+
+            if eventID == None:
+                eventID = event_list[0]["id"]
+        except:
+            eventID = 0
+
+        for item in event_list:
+
+            if item["id"] == int(eventID):
+                item["selected"] = True
+            else:
+                item["selected"] = False
+            print(item)
+
+        sql = "SELECT name, timeStamp FROM dataList WHERE chapterID = %s AND eventID = %s ORDER BY timeStamp DESC"
+        cursor.execute(sql, (session["chapterID"], eventID))
+        dataList = cursor.fetchall()
+
+        cursor.close()
+        connection.close()
+
+        for item in dataList:
+            item["timeStamp"] = item[
+                "timeStamp"].strftime("%I:%M %p %m/%d/%Y ")
+
+        return render_template('index.html', chapter=results["name"], email=results["email"],
+                               dataList=dataList, event_list=event_list, eventID=eventID)
     else:
         return render_template('login.html')
+
+
+@app.route("/add_event", methods=["POST"])
+def createEvent():
+
+    connection = createDBConnection()
+
+    name = request.form["event_name"]
+    chapterID = session["chapterID"]
+
+    cursor = connection.cursor()
+    sql = "INSERT INTO events(name, chapterID) VALUES(%s, %s)"
+    cursor.execute(sql, (name, chapterID))
+
+    cursor.execute("SELECT LAST_INSERT_ID()")
+    new_id = cursor.fetchone()["LAST_INSERT_ID()"]
+
+    cursor.close()
+    connection.commit()
+    connection.close()
+
+    returnDic = {"name": name, "url": new_id}
+
+    return json.dumps(returnDic), status.HTTP_202_ACCEPTED
+
+
+@app.route("/get_event", methods=["GET"])
+def getEvent():
+
+    event_id = request.args.get("event_id")
+
+    connection = createDBConnection()
+
+    cursor = connection.cursor()
+    sql = "SELECT name, timeStamp FROM dataList WHERE eventID=%s AND chapterID=%s"
+    cursor.execute(sql, (event_id, session["chapterID"]))
+    dataList = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    for item in dataList:
+        item["timeStamp"] = item[
+            "timeStamp"].strftime("%I:%M %p %m/%d/%Y ")
+
+    return json.dumps(dataList), status.HTTP_202_ACCEPTED
 
 
 @app.route("/login", methods=["POST"])
@@ -73,6 +173,9 @@ def login():
     sql = "SELECT id, password, chapterID FROM users WHERE email=%s"
     cursor.execute(sql, (email))
     results = cursor.fetchone()
+
+    cursor.close()
+    connection.close()
 
     validCredentials = False
 
@@ -105,6 +208,7 @@ def cardReader():
         studentID = request.form["studentID"]
         name = request.form["name"]
         raw = request.form["raw"]
+        eventID = request.form["eventID"]
     except:
         return "", status.HTTP_400_BAD_REQUEST
 
@@ -115,14 +219,29 @@ def cardReader():
 
     cursor = connection.cursor()
     sql = "INSERT INTO dataList(name, studentID, cardText, userID, " + \
-        "chapterID) VALUES(%s, %s, %s, %s, %s)"
-    cursor.execute(sql, (name, studentID, raw, userID, chapterID))
+        "chapterID, eventID) VALUES(%s, %s, %s, %s, %s, %s)"
+    cursor.execute(sql, (name, studentID, raw, userID, chapterID, eventID))
     cursor.close()
     connection.commit()
     connection.close()
 
-    return "", status.HTTP_202_ACCEPTED
+    returnDic = {"name": name, "time": datetime.now(
+    ).strftime("%I:%M %p %m/%d/%Y "), "blackList": ["No"]}
+
+    return json.dumps(returnDic), status.HTTP_202_ACCEPTED
+
+
+# reload the templates without restarting
+extra_dirs = ['templates']
+extra_files = extra_dirs[:]
+for extra_dir in extra_dirs:
+    for dirname, dirs, files in os.walk(extra_dir):
+        for filename in files:
+            filename = os.path.join(dirname, filename)
+            if os.path.isfile(filename):
+                extra_files.append(filename)
 
 if __name__ == "__main__":
     run_simple(config["website"], config["port"], app,
-               use_reloader=True, use_debugger=True, use_evalex=True)
+               use_reloader=True, use_debugger=True, use_evalex=True,
+               extra_files=extra_files)
